5cc8f353825a57522d56de3c52f0abe9401cc269
ChandreLesch <143355884+ChandreLesch@users.noreply.github.com>
2023-12-20 12:02:51 +0200

    This is a test

--- src/Weather.js
+++ src/Weather.js
@@ -11,4 +11,5 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
+  return <div>This is a test</div>;
 }
